2ccdcaba0fb2ba43ac570719683435d757fdb4cd
Maxwell Morris <48025321+maxmorrisio@users.noreply.github.com>
2023-06-14 20:39:44 +0100

    Tidy up

--- destroy.sh
+++ destroy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-printf "\nWARN - We are about to generate a destroy plan!\n"
+printf "\nWARN - We are about to generate a destroy plan!\n\n"
 read -p "DigitalOcean Token: " TF_VAR_do_token
 terraform destroy -auto-approve
