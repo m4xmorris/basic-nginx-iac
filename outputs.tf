08692b5b6d485e08b73837fe308a18a84674a24a
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2023-06-14 20:05:41 +0100

    Tidy up

--- outputs.tf
+++ outputs.tf
@@ -16,7 +16,7 @@ resource "null_resource" "ansible_inventory" {
 
   provisioner "local-exec" {
     command = <<-EOT
-      printf "[webservers:vars]\nansible_user=root\nansible_ssh_private_key_file=/Users/mhmorris/.ssh/morrislan.pub\nansible_ssh_common_args='-o StrictHostKeyChecking=no'\n[webservers]\n${join("\n", local.ansible_inventory.ip_addresses)}" > inventory.ini
+      printf "[webservers:vars]\nansible_user=root\nansible_ssh_private_key_file=/Users/mhmorris/.ssh/morrislan.pub\nansible_ssh_common_args='-o StrictHostKeyChecking=no'\n\n[webservers]\n${join("\n", local.ansible_inventory.ip_addresses)}" > inventory.ini
     EOT
   }
 }
