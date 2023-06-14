da80b06040fff1d0cd35f96fd6e01c37d5135a7b
Maxwell Morris <48025321+maxmorrisio@users.noreply.github.com>
2023-06-14 20:32:31 +0100

    Tidy up

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-read -p "DigitalOcean Token: " TF_VAR_do_token
-terraform init
+read -p "DigitalOcean Token: " TF_VAR_do_token; echo
+if [ ! -f .terraform.lock.hcl ]; then
+    INIT_RUN=true
+    terraform init
+fi
 terraform apply -auto-approve
-echo "Waiting 30 seconds for VMs to come online..."
-sleep 30
-ansible-playbook nginx.yaml --inventory=inventory.ini
+if [[ $INIT_RUN = "true" ]]; then
+    printf "\nWaiting 30 seconds for webservers to be ready...\n"
+    sleep 30
+fi
+ansible-playbook nginx.yaml --inventory="inventory.ini"
