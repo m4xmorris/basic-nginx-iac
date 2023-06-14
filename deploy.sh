7c91992682c9cfbc576c2a14a46244ac27ee04cd
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2023-06-14 20:04:06 +0100

    deploy.sh asks for DO token

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+read -p "DigitalOcean Token: " TF_VAR_do_token
 terraform init
-terraform apply -auto-approve -var="do_token"="dop_v1_60e5658d62b03b130110b5f3aa30204d6d43315c726662c1a77e07c2713e6ab7"
+terraform apply -auto-approve
 echo "Waiting for VM to come online..."
 sleep 30
 ansible-playbook nginx.yaml --inventory=inventory.ini
