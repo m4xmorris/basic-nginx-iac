dccdec64414d0c7dba17a6b97c56472742add992
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2023-06-14 20:07:54 +0100

    Fix

--- main.tf
+++ main.tf
@@ -9,5 +9,5 @@ resource "digitalocean_droplet" "webserver" {
 	image = "ubuntu-20-04-x64"
 	region = "lon1"
 	size = "s-1vcpu-1gb"
-	ssh_keys = [digitalocean_ssh_key.onepassword.id]
+	ssh_keys = [digitalocean_ssh_key.admin_key.id]
 }
